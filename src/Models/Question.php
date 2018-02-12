79fc75213654689676e3c71f25a28d5a9e60aa6a
Brian Faust <hello@brianfaust.me>
2018-02-12 06:46:12 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- src/Models/Question.php
+++ src/Models/Question.php
@@ -14,8 +14,8 @@ declare(strict_types=1);
 namespace BrianFaust\Questionable\Models;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Database\Eloquent\Relations\MorphTo;
 
 class Question extends Model
